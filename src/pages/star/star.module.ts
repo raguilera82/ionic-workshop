d60a6c3c3fd3229b80a4772636c06a1b263caa2f
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2018-01-19 00:36:35 +0100

    Component to render external HTML

--- src/pages/star/star.module.ts
+++ src/pages/star/star.module.ts
@@ -1,3 +1,4 @@
+import { MyTemplateComponent } from './../../components/my-template/my-template';
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { StarPage } from './star';
@@ -5,6 +6,7 @@ import { StarPage } from './star';
 @NgModule({
   declarations: [
     StarPage,
+    MyTemplateComponent
   ],
   imports: [
     IonicPageModule.forChild(StarPage),
